f2e3e595ec1edb29117f4929757b822484567a2d
github username <github email address>
2017-07-04 18:34:54 -0400

    still need to complete CLASS to pass tests

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save
+    db
+  end
+
+
 end
